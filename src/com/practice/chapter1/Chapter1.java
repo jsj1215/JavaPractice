5d30364c3f86a88f12c6945e7a3bd56d89be7b59
jsj1215 <jsj1215@naver.com>
2021-04-05 23:13:38 +0900

    zzzz

--- src/com/practice/chapter1/Chapter1.java
+++ src/com/practice/chapter1/Chapter1.java
@@ -4,12 +4,12 @@ public class Chapter1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 2021.04.05
 		System.out.println("Hello,Java");
 		
 		//Ctrl + S 누르는 순간 컴파일 됨 -> .Class 생김.
 		//Ctrl + F11
 		//컴파일 된 Class 파일 위치 -> bin 폴더 안에 생김.
-		
 		
 	}
 
