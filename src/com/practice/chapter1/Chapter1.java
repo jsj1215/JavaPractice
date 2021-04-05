a3a155b623fa990675273a97b8ff2873b5720f68
unknown <jsj1215@naver.com>
2021-04-05 22:34:37 +0900

    sujin first commit

---
+++ src/com/practice/chapter1/Chapter1.java
@@ -0,0 +1,15 @@
+package com.practice.chapter1;
+
+public class Chapter1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello,Java");
+		
+		//Ctrl + S 누르는 순간 컴파일 됨 -> .Class 생김.
+		//Ctrl + F11
+		//컴파일 된 Class 파일 위치 -> bin 폴더 안에 생김.
+		
+	}
+
+}
